68a0879b8dace86434d901abb90112e168095e3f
子长 <zichang.nong@tuya.com>
2022-03-01 10:09:47 +0800

    ci: jest --coverage

--- tests/selector/config.js
+++ tests/selector/config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  test() {
-    return true;
-  },
-  selector: { tags: true, classes: true },
+  test: /[-a-z]+/,
+  selector: { tags: true, classes: true, ids: true },
   rename(value) {
     if (value === 'page') {
       return 'body';
